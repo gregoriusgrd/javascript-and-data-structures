bad2e94b18ec58c6da5f644a22a743e34af6c06c
gregoriusgrd <gregorius.gerald21@gmail.com>
2025-02-06 22:38:06 +0700

    add buy weapon

--- role-playing-game/script.js
+++ role-playing-game/script.js
@@ -18,6 +18,25 @@ const monsterStats = document.querySelector("#monsterStats");
 const monsterName = document.querySelector("#monsterName");
 const monsterHealthText = document.querySelector("#monsterHealth");
 
+const weapons = [
+    {
+      name: "stick",
+      power: 5
+    },
+    {
+      name: "dagger",
+      power: 30
+    },
+    {
+      name: "claw hammer",
+      power: 50
+    },
+    {
+      name: "sword",
+      power: 100
+    }
+  ];
+
 const locations = [
     {
         name: "town square",
@@ -70,12 +89,37 @@ function fightDragon () {
     console.log("Fighting dragon.") 
 }
 
-function buyHealth () {
+function buyHealth() {
+    if (gold >= 10) {
+        gold -= 10;
+        health += 10;
+        goldText.innerText = gold;
+        healthText.innerText = health;
+    } else {
+        text.innerText = "You do not have enough gold to buy health."; 
+    }
+  }
+
+if (currentWeaponIndex <= 3) {
     
 }
 
 function buyWeapon () {
-
+    if (currentWeaponIndex < weapons.length - 1) {
+        if (gold >= 30) {
+            gold -= 30;
+            currentWeaponIndex++;
+            goldText.innerText = gold;
+            let newWeapon = weapons[currentWeaponIndex].name;
+            text.innerText = "You now have a " + newWeapon + ".";
+            inventory.push(newWeapon);
+            text.innerText += " In your inventory you have: " + inventory;
+        } else {
+            text.innerText = "You do not have enough gold to buy a weapon.";
+        }
+    } else {
+        text.innerText = "You already have the most powerful weapon!";
+    }
 }
 
 function fightSlime () {
