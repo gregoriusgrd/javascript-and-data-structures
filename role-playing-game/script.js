74e8cdd2b75634690e819bf66f3db2a51e71e307
gregoriusgrd <gregorius.gerald21@gmail.com>
2025-02-06 19:59:45 +0700

    add locations

--- role-playing-game/script.js
+++ role-playing-game/script.js
@@ -16,13 +16,31 @@ const healthText = document.querySelector("#healthText");
 const goldText = document.querySelector("#goldText");
 const monsterStats = document.querySelector("#monsterStats");
 const monsterName = document.querySelector("#monsterName");
-
 const monsterHealthText = document.querySelector("#monsterHealth");
+
+const locations = [
+    {
+        name: "town square",
+        "button text": ["Go to store", "Go to cave", "Fight dragon"],
+        "button functions": [goStore, goCave, fightDragon],
+        text: "You are in the town square. You see a sign that says \"Store\"."
+    },
+    {
+        name: "store",
+        "button text": ["Buy 10 health (10 gold)", "Buy weapon (30 gold)", "Go to town square"],
+        "button functions": [buyHealth, buyWeapon, goTown],
+        text: "You enter the store."
+    }
+];
 
 // initialize buttons
 button1.onclick = goStore;
 button2.onclick = goCave;
 button2.onclick = fightDragon;
+
+function update (location) {
+
+}
 
 function goTown () {
     button1.innerText = "Go to store"
