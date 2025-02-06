737260c749ccfa1eec1854087471f2e609f69b16
gregoriusgrd <gregorius.gerald21@gmail.com>
2025-02-06 19:25:24 +0700

    add go store function

--- role-playing-game/script.js
+++ role-playing-game/script.js
@@ -18,3 +18,30 @@ const monsterStats = document.querySelector("#monsterStats");
 const monsterName = document.querySelector("#monsterName");
 
 const monsterHealthText = document.querySelector("#monsterHealth");
+
+function goStore () {
+    button1.innerText = "Buy 10 health (10 gold)"
+    button2.innerText = "Buy weapon (30 gold)"
+    button3.innerText = "Go to town square"
+
+    button1.onclick = buyHealth;
+    button2.onclick = buyWeapon;
+    button3.onclick = goTown;
+    
+    text.innerText = "You enter the store."
+
+}
+
+
+function goCave () {
+    console.log("Going to cave.") 
+}
+
+function fightDragon () {
+    console.log("Fighting dragon.") 
+}
+
+// initialize buttons
+button1.onclick = goStore;
+button2.onclick = goCave;
+button2.onclick = fightDragon;
