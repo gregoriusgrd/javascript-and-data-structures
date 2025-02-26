0f7cb46af699e30715a959c2397f1fa8947242f8
gregoriusgrd <gregorius.gerald21@gmail.com>
2025-02-26 23:00:45 +0700

    Initial commit

---
+++ variables-and-strings/sentence-maker/script.js
@@ -0,0 +1,51 @@
+let adjective = "hungry";
+let noun = "dragon";
+let verb = "angry";
+let place = "cave";
+let adjective2 = "small";
+let noun2 = "human";
+
+const firstStory =
+  "Once upon a time, there was a(n) " +
+  adjective +
+  " " +
+  noun +
+  " who loved to eat " +
+  noun2 +
+  ". The " +
+  noun +
+  " lived in a " +
+  place +
+  " and had " +
+  adjective2 +
+  " nostrils that blew fire when it was " +
+  verb +
+  ".";
+
+console.log("First story: " + firstStory);
+
+adjective = "big";
+noun = "golem";
+verb = "attacked";
+place = "ruin";
+adjective2 = "small";
+noun2 = "plants";
+
+const secondStory =
+  "Once upon a time, there was a(n) " +
+  adjective +
+  " " +
+  noun +
+  " who loved to eat " +
+  noun2 +
+  ". The " +
+  noun +
+  " lived in a " +
+  place +
+  " and had " +
+  adjective2 +
+  " nostrils that blew fire when it was " +
+  verb +
+  ".";
+
+console.log("Second story: " + secondStory);
