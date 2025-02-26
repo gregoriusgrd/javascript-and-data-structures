9a64a2004e2c79c6aa6354a9a28e667aec00646e
gregoriusgrd <gregorius.gerald21@gmail.com>
2025-02-26 22:27:10 +0700

    Initial commit

---
+++ variables-and-strings/trivia-bot/script.js
@@ -0,0 +1,33 @@
+console.log("Hello! I'm your coding fun fact guide!");
+
+let botName = "SkyBot";
+let botLocation = "Jakarta";
+let favoriteLanguage = "JavaScript";
+
+console.log("My name is " + botName + " and I live on " + botLocation + ".");
+
+console.log("My favorite programming language is " + favoriteLanguage + ".");
+
+let codingFact =
+  "Fun Fact: " +
+  favoriteLanguage +
+  " is one of the most popular programming languages.";
+
+console.log(codingFact);
+
+codingFact = "The real name is not " + favoriteLanguage + ", but ECMAScript.";
+
+console.log(codingFact);
+
+codingFact =
+  favoriteLanguage + " is influenced by languages such as C and Java.";
+
+console.log(codingFact);
+
+console.log(
+  "It was fun sharing these facts with you. Goodbye! - " +
+    botName +
+    " from " +
+    botLocation +
+    "."
+);
