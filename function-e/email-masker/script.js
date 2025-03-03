2a68ec4b53db87fc058909395113de2a5f354a09
gregoriusgrd <gregorius.gerald21@gmail.com>
2025-03-03 22:22:02 +0700

    update

--- function-e/email-masker/script.js
+++ function-e/email-masker/script.js
@@ -4,4 +4,14 @@ const maskEmail = (email) => {
   const atIndex = email.indexOf("@");
   const localPart = email.slice(0, atIndex);
   const domain = email.slice(atIndex);
+
+  const firstChar = localPart[0];
+  const lastChar = localPart[localPart.length - 1];
+  const maskedMiddle = "*".repeat(localPart.length - 1);
+
+  return firstChar + maskedMiddle + lastChar + domain;
 };
+
+console.log(maskEmail("apple.pie@example.com"));
+
+console.log(maskEmail("freecodecamp@example.com"));
