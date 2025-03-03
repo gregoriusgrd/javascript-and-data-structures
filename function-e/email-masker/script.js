4efd29aed01c9fe3b2e3c3e861636a7bdea886d1
gregoriusgrd <gregorius.gerald21@gmail.com>
2025-03-03 22:17:07 +0700

    update code

--- function-e/email-masker/script.js
+++ function-e/email-masker/script.js
@@ -1,8 +1,7 @@
 let email = "freecodecamp@example.com";
 
 const maskEmail = (email) => {
-  const theMask = email.indexOf("@");
-  const firstChar = email.slice(0, theMask);
-  const secondChar = email.slice(-1);
-  let replace = firstChar.replace("*");
+  const atIndex = email.indexOf("@");
+  const localPart = email.slice(0, atIndex);
+  const domain = email.slice(atIndex);
 };
