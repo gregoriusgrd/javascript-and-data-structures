37605d54bcb29974ffa0b5fe3948422f58cfa592
gregoriusgrd <gregorius.gerald21@gmail.com>
2025-03-04 19:35:02 +0700

    added more function

--- array/lunch-picker/script.js
+++ array/lunch-picker/script.js
@@ -6,8 +6,60 @@ function addLunchToEnd(array, string) {
   return array;
 }
 
+addLunchToEnd(lunches, "Tacos");
+console.log(lunches);
+
+addLunchToEnd(["Pizza", "Tacos"], "Burger");
+console.log(lunches);
+
 function addLunchToStart(array, string) {
   array.unshift(string);
   console.log(`${string} added to the start of the lunch menu.`);
   return array;
 }
+
+addLunchToStart(lunches, "Milk");
+console.log(lunches);
+
+addLunchToStart(["Burger", "Sushi"], "Pizza");
+console.log(lunches);
+
+function removeLastLunch(array) {
+  let removedLastLunch;
+  if (array.length === 0) {
+    console.log("No lunches to remove.");
+  } else {
+    removedLastLunch = array.pop(lunches);
+    console.log(`${removedLastLunch} removed from the end of the lunch menu.`);
+  }
+  return array;
+}
+
+removeLastLunch(lunches);
+console.log(lunches);
+
+function removeFirstLunch(array) {
+  let removedFirstLunch;
+  if (array.length === 0) {
+    console.log("No lunches to remove.");
+  } else {
+    removedFirstLunch = array.shift(lunches);
+    console.log(
+      `${removedFirstLunch} removed from the start of the lunch menu.`
+    );
+  }
+  return array;
+}
+
+function getRandomLunch(array) {
+  let randomLunch;
+  if (array.length === 0) {
+    console.log(`No lunches available.`);
+  } else {
+    randomLunch = array[Math.floor(Math.random() * array.length)];
+    console.log(`Randomly selected lunch: ${randomLunch}`);
+  }
+  return randomLunch;
+}
+
+getRandomLunch(lunches);
