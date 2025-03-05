55f821994d10646af0ae8515f82ebb323eed033a
gregoriusgrd <gregorius.gerald21@gmail.com>
2025-03-06 01:41:17 +0700

    Initial commit

---
+++ objects/lunch-tracker/script.js
@@ -0,0 +1,39 @@
+const recipes = [];
+
+const recipe1 = {
+  name: "Spaghetti Carbonara",
+  ingredients: ["spaghetti", "Parmesan cheese", "pancetta", "black pepper"],
+  cookingTime: 22,
+  totalIngredients: null,
+  difficultyLevel: "",
+  ratings: [4, 5, 4, 5],
+  averageRating: null,
+};
+
+const recipe2 = {
+  name: "Chicken Curry",
+  ingredients: [
+    "chicken breast",
+    "coconut milk",
+    "curry powder",
+    "onion",
+    "garlic",
+  ],
+  cookingTime: 42,
+  totalIngredients: null,
+  difficultyLevel: "",
+  ratings: [4, 5, 5, 5],
+  averageRating: null,
+};
+
+const recipe3 = {
+  name: "Vegetable Stir Fry",
+  ingredients: ["broccoli", "carrot", "bell pepper"],
+  cookingTime: 15,
+  totalIngredients: null,
+  difficultyLevel: "",
+  ratings: [4, 3, 4, 5],
+  averageRating: null,
+};
+
+recipes.push(recipe1, recipe2, recipe3);
