e10a4894d7a1e10db68f2031c4ac3e57ca84e1a7
gregoriusgrd <gregorius.gerald21@gmail.com>
2025-03-06 15:08:34 +0700

    Initial commit

---
+++ objects/quiz-game/script.js
@@ -0,0 +1,37 @@
+const questions = [
+  {
+    category: "Space",
+    question: "Which planet is known as the Red Planet?",
+    choices: ["a. Venus", "b. Mars", "c. Saturn"],
+    answer: "b. Mars",
+  },
+  {
+    category: "Music",
+    question: "Which pop singer is known as the King of Pop?",
+    choices: ["a. Elvis Presley", "b. Michael Jackson", "c. Justin Bieber"],
+    answer: "b. Michael Jackson",
+  },
+  {
+    category: "Sports",
+    question:
+      "How many players are there in a standard soccer team on the field?",
+    choices: ["a. 9", "b. 10", "c. 11"],
+    answer: "c. 11",
+  },
+  {
+    category: "Technology",
+    question: "What does CPU stand for?",
+    choices: [
+      "a. Central Processing Unit",
+      "b. Computer Personal Unit",
+      "c. Central Peripheral Unit",
+    ],
+    answer: "a. Central Processing Unit",
+  },
+  {
+    category: "Geography",
+    question: "Which country has the largest land area?",
+    choices: ["a. Russia", "b. China", "c. United States"],
+    answer: "a. Russia",
+  },
+];
